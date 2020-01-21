9d34bad8adffcddbbe942afbe1ec68d66b9bdd44
Katherine Rose Wolf <katherinerosewolf@gmail.com>
2020-01-20 22:40:57 -0800

    attempted to break rmds into separate jobs to avoid timeout on Travis

---
+++ tests/render_notebook_one.R
@@ -0,0 +1,3 @@
+
+## call rmarkdown on first notebook
+rmarkdown::render(input = "notebook_one_demog_air_clean.Rmd")
---
+++ tests/render_notebook_two.R
@@ -0,0 +1,3 @@
+
+## call rmarkdown on second notebook
+rmarkdown::render(input = "notebook_two_monitor_work.Rmd")
